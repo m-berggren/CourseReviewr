dcf3fba468347378eb81ebb604d58c04c5d28696
Jerome <zhaoyuw@net.chalmers.se>
2024-09-11 14:42:00 +0200

    #9 Update file with correct content after previous empty push

---
+++ server/models/courseList.js
@@ -0,0 +1,13 @@
+const {Schema, model} = require('mongoose');
+const {v4: uuidv4} = require('uuid');
+
+const courseListSchema = new Schema({
+    courseListID: {type: String, default: uuidv4, unique: true, index: true},
+    name: {type: String, default: 'Untitled List'},
+    creationDate: {type: Date, default: Date.now},
+    creator: {type: String, ref: 'User', required: true},
+    description: String,
+    courses: [{type: String, ref: 'Course', default: []}],
+})
+
+module.exports = model('CourseList', courseListSchema);
