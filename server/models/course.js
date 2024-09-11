351180c1a20c697a3ccd9a561b9d1dee36595968
Marcus Berggren <marbergg@student.chalmers.se>
2024-09-11 16:12:51 +0200

    #10 Update Course and User schema syntax and visibility

--- server/models/course.js
+++ server/models/course.js
@@ -2,18 +2,61 @@ const { Schema, model } = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const courseSchema = new Schema({
-    courseID: { type: String, default: uuidv4, unique: true, index: true },
-    name: String,
-    topic: [String],
-    difficulty: String,
-    description: String,
-    averageRating: Number,
-    releaseYear: Number,
-    provider: String,
-    certificate: String,
-    availability: String,
-    reviews: [{ type: Schema.Types.ObjectId, ref: 'Review', default: [] }],
-    photo: { type: String, default: null }
+    courseID: {
+        type: String,
+        default: uuidv4,
+        unique: true,
+        index: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    topic: {
+        type: [String],
+        required: true,
+        trim: true
+    },
+    difficulty: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        default: null
+    },
+    averageRating: {
+        type: Number,
+        default: null
+    },
+    releaseYear: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    provider: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    certificate: {
+        type: String,
+        default: null
+    },
+    availability: {
+        type: String,
+        default: null
+    },
+    reviews: [{
+        type: Number,
+        ref: 'Review',
+        default: []
+    }],
+    photo: {
+        type: String,
+        default: null
+    }
 });
 
 module.exports = model('Course', courseSchema);
