8e25764065aaab0210729bf5830a3f01011dc956
Marcus Berggren <marbergg@student.chalmers.se>
2024-09-20 13:31:57 +0200

    #21 Add additional attributes for Review and handling in reviews router

--- server/models/review.js
+++ server/models/review.js
@@ -5,13 +5,21 @@ const { Schema, model } = mongoose;
 const AutoIncrement = AutoIncrementFactory(mongoose);
 
 const reviewSchema = new Schema({
-    reviewID:       { type: Number, unique: true, index: true },
-    user:           { type:Number, ref : 'User', required: [true, 'UserID is required'] },
-    course:         { type: Number, ref: 'Course', required: [true, 'CourseID is required'] },
-    rating:         [{ type: Number, required: [true, 'Rating is required'], min : 1, max: 5} ],
-    date:           { type: Date, default: Date.now },
-    comment:        { type: String, default: '' },
-    hasCompleted:   { type: Boolean }
+    reviewID:               { type: Number, unique: true, index: true },
+    user:                   { type: Number, ref : 'User', required: [true, 'UserID is required'] },
+    course:                 { type: Number, ref: 'Course', required: [true, 'CourseID is required'] },
+    engagementLevel:        { type: Number, required: [true, 'Rating is required'], min: 1, max: 5 },
+    practicalValue:         { type: Number, required: [true, 'Rating is required'], min: 1, max: 5 },
+    instructorQuality:      { type: Number, required: [true, 'Rating is required'], min: 1, max: 5 },
+    difficultyLevel:        { type: Number, required: [true, 'Rating is required'], min: 1, max: 5 },
+    comment:                { type: String, default: '' },
+    hasCompleted:           { type: Boolean },
+    date:                   { type: Date, default: Date.now }
+});
+
+// Virtual field for calculating average rating
+reviewSchema.virtual('averageRating').get(function () {
+    return (( this.engagementLevel + this.practicalValue + this.instructorQuality + this.difficultyLevel )/4 );
 });
 
 reviewSchema.index({user:1, course:1},{unique:true});
