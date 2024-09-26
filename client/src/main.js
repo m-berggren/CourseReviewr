b6f55ab3c0f1e2d6508160835086e8f8df720cf8
Marcus Berggren <marbergg@student.chalmers.se>
2024-09-26 23:22:21 +0200

    #26 Include star-rating and quick-fix api versioning

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,15 @@ import { createApp } from 'vue'
 import { createBootstrap } from 'bootstrap-vue-next'
 import App from './App.vue'
 import router from './router'
+import StarRating from 'vue-star-rating'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const app = createApp(App)
+
+app.component('star-rating', StarRating)
+
 app.use(createBootstrap())
 app.use(router)
 app.mount('#app')
