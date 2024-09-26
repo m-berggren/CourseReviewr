4dd73681106e9bee7bacb176b12ae7870a140928
Marcus Berggren <marbergg@student.chalmers.se>
2024-09-26 23:23:45 +0200

    #26 Create VUE files for Courses and component CourseItem and add its route

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Homepage from './views/Homepage.vue'
+import Courses from './views/Courses.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Homepage }
+  { path: '/', name: 'home', component: Homepage },
+  { path: '/courses', name: 'courses', component: Courses }
 ]
 
 const router = createRouter({
