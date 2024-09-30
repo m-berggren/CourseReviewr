95c2e632d9cfbb77aa94b792bb4255020166eab7
Marcus Berggren <marbergg@student.chalmers.se>
2024-09-30 13:09:16 +0200

    #30 Prettify resizing on homepage

--- client/src/views/Homepage.vue
+++ client/src/views/Homepage.vue
@@ -3,9 +3,8 @@
       <b-container fluid>
 
         <!-- Row for welcome text, search bar and tags -->
-        <b-row class="top-row">
-          <b-col></b-col>
-          <b-col>
+        <b-row class="top-row justify-content-center text-center">
+          <b-col md="8" lg="6">
             <h1 class="mt-2">Welcome!</h1>
             <p>Find your perfect course or add your own course with review.</p>
             <b-input-group>
@@ -15,53 +14,42 @@
               </b-input-group-append>
             </b-input-group>
           </b-col>
-          <b-col></b-col>
         </b-row>
 
         <!-- Row for tags inside a container -->
-        <b-row class="top-row mb-4">
-          <b-col></b-col>
-          <b-col md="4">
+        <b-row class="top-row mb-4 justify-content-center text-center">
+          <b-col md="6">
             <div class="tags-container p-3 mt-2 mb-4">
               <b-badge v-for="topic in topics" :key="topic._id" variant="dark" class="tag-badge mx-1">
                 {{ topic.name }}
               </b-badge>
             </div>
           </b-col>
-          <b-col></b-col>
         </b-row>
 
         <!-- Rows for course header and courses (4x3 layout) -->
-         <b-row class="">
-          <b-col></b-col>
-          <b-col>
+         <b-row class="justify-content-center text-center">
+          <b-col md="8">
             <h2 class="">Recommended courses</h2>
           </b-col>
-          <b-col></b-col>
          </b-row>
 
-        <b-row>
+        <b-row class="justify-content-center">
           <b-col md="3" v-for="course in courses" :key="course._id">
             <course-item v-bind:course="course"></course-item>
           </b-col>
         </b-row>
 
         <!-- Rows for review header and reviews (4x3 layout)-->
-         <b-row class="">
-          <b-col></b-col>
-          <b-col>
+         <b-row class="justify-content-center text-center">
+          <b-col md="8">
             <h2 class="mt-1">Latest reviews</h2>
           </b-col>
-          <b-col></b-col>
          </b-row>
 
-        <b-row>
+        <b-row class="justify-content-center">
           <b-col md="3" v-for="review in reviews" :key="review._id">
-            <b-card class="my-3">
-              <b-card-text>
-                {{ review.comment }}
-              </b-card-text>
-            </b-card>
+            <review-item v-bind:review="review"></review-item>
           </b-col>
         </b-row>
       </b-container>
@@ -71,12 +59,14 @@
 
 <script>
 import CourseItem from '../components/CourseItem.vue'
+import ReviewItem from '../components/ReviewItem.vue'
 import { Api } from '@/Api'
 
 export default {
   name: 'topics-courses-and-reviews',
   components: {
-    'course-item': CourseItem
+    'course-item': CourseItem,
+    'review-item': ReviewItem
   },
   mounted() {
     Api.get('/topics')
