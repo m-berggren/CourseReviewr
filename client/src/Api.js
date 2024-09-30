3b1ffc97298a7f02986edf81532da1dba764ce7d
Lian Shi <lians@student.chalmers.se>
2024-09-30 22:48:33 +0200

    #31 Fix token decoding and fetching user data from token

--- client/src/Api.js
+++ client/src/Api.js
@@ -1,5 +1,21 @@
 import axios from 'axios'
+import { token } from './token'
 
-export const Api = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_ENDPOINT || 'http://localhost:3000/api/v1'
 })
+
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = token.getToken()
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+export const Api = api
--- client/src/token.js
+++ client/src/token.js
@@ -1,16 +1,37 @@
+import { jwtDecode } from 'jwt-decode'
+
 class TokenHelper {
   constructor() {
     this.token = localStorage.getItem('token')
+    this.decodedToken = jwtDecode(this.token)
   }
 
   set(token) {
     localStorage.setItem('token', token)
     this.token = token
+    this.decodedToken = jwtDecode(token)
   }
 
   unset() {
     localStorage.removeItem('token')
     this.token = null
+    this.decodedToken = null
+  }
+
+  getUserId() {
+    return this.decodedToken ? this.decodedToken.id : null
+  }
+
+  getUsername() {
+    return this.decodedToken ? this.decodedToken.username : null
+  }
+
+  getRole() {
+    return this.decodedToken ? this.decodedToken.role : null
+  }
+
+  getToken() {
+    return this.token
   }
 
   getOrThrow() {
