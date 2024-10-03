c69b662a1c8803021aa6a91aa2d33900e6317c92
Lian Shi <lians@student.chalmers.se>
2024-10-03 16:28:40 +0200

    #31 Comment out buffer image storage method

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,16 +4,22 @@ import validator from 'validator';
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-    username:           { type: String, required: [true, 'Username is required'], unique: true, trim: true },
-    email:              { type: String, required: [true, 'Email is required'], unique: true, trim: true, validate: {
-        validator: validator.isEmail, message: 'invalid email' } },
-    password:           { type: String, required: [true, 'Password is required'] },
-    photo:              { data: Buffer, contentType: String },
-    interests:          { type: [Schema.Types.ObjectId], ref: 'Topic', default: [], 
-        set: v => Array.from(new Set(v.map(interest => interest.toString()))) },
+    username: { type: String, required: [true, 'Username is required'], unique: true, trim: true },
+    email: {
+        type: String, required: [true, 'Email is required'], unique: true, trim: true, validate: {
+            validator: validator.isEmail, message: 'invalid email'
+        }
+    },
+    password: { type: String, required: [true, 'Password is required'] },
+    //photo:              { data: Buffer, contentType: String },
+    photo: { type: String, default: null },
+    interests: {
+        type: [Schema.Types.ObjectId], ref: 'Topic', default: [],
+        set: v => Array.from(new Set(v.map(interest => interest.toString())))
+    },
     recommendationList: [{ type: Schema.Types.ObjectId, ref: 'Course', default: [] }],
-    courseLists:        [{ type: Schema.Types.ObjectId, ref: 'CourseList', default: [] }],
-    role:               {type : String, enum: ['user', 'admin'], default: 'user'}
+    courseLists: [{ type: Schema.Types.ObjectId, ref: 'CourseList', default: [] }],
+    role: { type: String, enum: ['user', 'admin'], default: 'user' }
 });
 
 export default model('User', userSchema);
