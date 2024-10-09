037a3ce569c17ef43c3f2ac1d19f35d28e350b0b
Lian Shi <lians@student.chalmers.se>
2024-10-09 14:01:54 +0200

    #34 Implement redirecting user to sign in after token expires

--- client/src/router.js
+++ client/src/router.js
@@ -8,21 +8,31 @@ import Profile from './views/Profile.vue'
 import CreateCourse from './views/CreateCourse.vue'
 import CourseLists from './views/CourseLists.vue'
 import Coursepage from './views/Coursepage.vue'
+import { token } from './token'
 
 const routes = [
   { path: '/', name: 'home', component: Homepage },
   { path: '/signin', name: 'signin', component: Signin },
   { path: '/courses', name: 'courses', component: Courses },
   { path: '/register', name: 'register', component: Register },
-  { path: '/profile', name: 'profile', component: Profile },
-  { path: '/courses/create', name: 'create-course', component: CreateCourse },
-  { path: '/course-lists', name: 'courselists', component: CourseLists },
+  { path: '/profile', name: 'profile', component: Profile, meta: { requiresAuth: true } },
+  { path: '/courses/create', name: 'create-course', component: CreateCourse, meta: { requiresAuth: true } },
+  { path: '/course-lists', name: 'courselists', component: CourseLists, meta: { requiresAuth: true } },
   { path: '/courses/:id', name: 'course-page', component: Coursepage }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const isSignedIn = token.isSignedIn()
+  if (to.meta.requiresAuth && !isSignedIn) {
+    next({ name: 'signin' })
+  } else {
+    next()
+  }
 })
 
 export default router
